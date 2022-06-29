93e234d41623d2a448e2b1412da58715743c6afe
siddhartha shandilya <siddharthashandilya104@gmail.com>
2022-06-29 18:36:09 +0530

    Applied log file and directed data source  to local storage

--- src/stage_01_retrieve_html.py
+++ src/stage_01_retrieve_html.py
@@ -1,64 +1,63 @@
-# importing necessary modules
-import requests
-from io import BytesIO
-import zipfile
-import os
-import time 
-import argparse
-import requests
-import sys
-from src.utils.all_utils import create_directory, read_yaml
+from src.utils.all_utils import read_yaml, create_directory
+import argparse, os, shutil
+import pandas as pd
+from tqdm import tqdm
+import logging
 
-def retrieve_html(config_path):
+
+#-------------------------------------------------------------------------------
+#   Creating a looging with timestamp and regex 
+#-------------------------------------------------------------------------------
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir, 'stage_01_running_logs.log'), level=logging.INFO, format=logging_str,filemode="a")
+
+#-------------------------------------------------------------------------------
+#   copy_file
+#-------------------------------------------------------------------------------
+
+def copy_file(source_download_dir, data_local_dir_path): 
+    list_of_files = os.listdir(source_download_dir)
+    N = len(list_of_files)
+    logging.info(f"\n\n location of lacal{ data_local_dir_path}")
+
+    for file in tqdm(list_of_files, total=N, desc= "copying file from {} to {}".format(source_download_dir, data_local_dir_path), colour="green"):
+        src = os.path.join(source_download_dir, file)
+        dest = os.path.join(data_local_dir_path, file)
+        shutil.copy(src, dest)
+
+#-------------------------------------------------------------------------------
+#   get_data
+#-------------------------------------------------------------------------------
+
+def get_data(config_path):
 
     config = read_yaml(config_path)
-
-    site_name = config["Data"]['site_name']
-    #city_code = config["Data"]['city_code']
-
+    source_download_dirs = config["Data"]["local_source_download_path"]
     artifacts_dir = config["artifacts"]['artifacts_dir']
     data_local_dir = config["artifacts"]['data_local_dir']
     raw_data_dir = config["artifacts"]['raw_data_dir']
+
     #html_data_dir = config["artifacts"]['html_data_dir']
 
-    data_local_dir_path = os.path.join(artifacts_dir, data_local_dir)
+    data_local_dir_path = os.path.join(artifacts_dir, data_local_dir, raw_data_dir )
 
-    create_directory(dirs= [data_local_dir_path])
+    #for source_download_dir, data_local_dir_path in tqdm(zip(source_download_dirs, data_local_dir_path), total=2, desc="list of folders", colour="red"):
+    create_directory([data_local_dir_path])
+    copy_file(source_download_dirs, data_local_dir_path)
 
-    dementia_local_dir_path = os.path.join(data_local_dir_path, raw_data_dir)
 
-    #print("#"*20 + " dementia_local_dir_path " + "#"*20)
-    #print(dementia_local_dir_path)
-    #print("#"*20 + " dementia_local_dir_path " + "#"*20)
-   
-    
-    #we are going to fetch dta from the site
-    print('Downloading started')
-    
-    # Downloading the file by sending the request to the URL
-    req = requests.get(site_name)
-    print('Downloading Completed')
-
-    # extracting the zip file contents
-    zip_file= zipfile.ZipFile(BytesIO(req.content))
-    print("##"*10 +"  location of downloaded unzipped file {} \n" .format( dementia_local_dir_path))
-    zip_file.extractall(dementia_local_dir_path)
-    
-    
-    
 if __name__=="__main__":
-
     args = argparse.ArgumentParser()
-
-    args.add_argument("--config", "-c", default="config/config.yaml")
-
+    args.add_argument("--config","-c", default="config/config.yaml")
     parsed_args = args.parse_args()
-
-    start_time=time.time()
-
-    retrieve_html(config_path=parsed_args.config)
-
-    stop_time=time.time()
-    print("time taken{}".format(stop_time-start_time))
- 
- 
+    
+    try:
+        logging.info("starting StGE 01 LOADING DATA")
+        get_data(config_path = parsed_args.config)
+        logging.info("Stage 01 completed successfully and all the data is saved in the locals")
+    except Exception as e:
+        logging.info(e)
+        raise e
