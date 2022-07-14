263ece142a4dd1c8c24fa451bf18aa705ad276cf
SiddharthaShandilya <siddharthashandilya104@gmail.com>
2022-07-14 13:34:28 +0530

    accuracy score in testing.json file for loaded model

--- src/stage_05_model_accuracy_testing.py
+++ src/stage_05_model_accuracy_testing.py
@@ -1,14 +1,14 @@
-from src.utils.all_utils import read_yaml, create_directory, load_model, error_value
+from src.utils.all_utils import read_yaml, create_directory, save_model, error_value, save_reports, load_model
 import os, argparse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
-import xgboost as xgb
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, accuracy_score, confusion_matrix
 import logging
 import io
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 
 
@@ -22,10 +22,20 @@ os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(filename=os.path.join(log_dir, 'stage_05_running_logs.log'), level=logging.INFO, format=logging_str,filemode="a")
 
 #-------------------------------------------------------------------------------
+#   Evaluting Model Metrics
+#-------------------------------------------------------------------------------
+
+def evaluate_metrics(actual_values, predicted_values):
+    rmse = np.sqrt(mean_squared_error(actual_values, predicted_values))
+    mae = mean_absolute_error(actual_values, predicted_values)
+    r2 = r2_score(actual_values, predicted_values)
+    return rmse, mae, r2
+
+#-------------------------------------------------------------------------------
 #   Testing Model 
 #-------------------------------------------------------------------------------
 
-def model_testing(model, test_data_file, graphs_dir_path):
+def model_testing(model, test_data_file, graphs_dir_path, scores_filepath):
 
     df = pd.read_csv(test_data_file)
     logging.info(df.head(10))
@@ -42,6 +52,9 @@ def model_testing(model, test_data_file, graphs_dir_path):
     logging.info(" fetched file location is : {}".format(test_data_file))
     logging.info("model.best_params_ : {}".format(model.best_params_))
     logging.info("model.best_score_ : {}".format(model.best_score_))
+    y_pred=[0 if i<0.6 else 1 for i in prediction]
+    logging.info("models confusion_matrix : {}".format(confusion_matrix(y,y_pred)))
+    logging.info("models accuracy_score : {}".format(accuracy_score(y,y_pred)))
 
     sns.displot(y-prediction)
     fig = plt.figure()
@@ -51,6 +64,29 @@ def model_testing(model, test_data_file, graphs_dir_path):
 
     #score =accuracy_score(y,prediction)
     #logging.info(score)
+    
+    rmse, mae, r2 = evaluate_metrics(y_pred, prediction)
+
+    model_best_params_  = model.best_params_
+    model_best_score_  = model.best_score_
+    y_pred=[0 if i<0.6 else 1 for i in prediction]
+    
+    
+    scores = {
+        "model.best_params_ ": model_best_params_,
+        "model.best_score_ ": model_best_score_ ,
+        "rmse": rmse,
+        "mae": mae,
+        "r2": r2,
+        "testing_accuracy_score": accuracy_score(y,y_pred),
+    }
+
+    save_reports(report=scores, report_path=scores_filepath)
+    logging.info(f"SCORES DATA: {scores}")
+    logging.info(f"Calculation error values")
+    error_value(model, X, y)
+    logging.info(f"Calculation  of error values ")
+
 
     
 
@@ -74,6 +110,12 @@ if __name__=="__main__":
     test_data_file = config["artifacts"]["split_data"]["test_data_file"]
     saved_model_filename = config["artifacts"]["model"]["xgboost_reg"]
 
+    scores_dir = config["artifacts"]["reports"]["reports_dir"]
+    scores_filename = config["artifacts"]["reports"]["testing_scores"]
+    scores_dir_path = os.path.join(artifacts_dir, scores_dir)
+    create_directory([scores_dir_path])
+    scores_filepath = os.path.join(scores_dir_path, scores_filename)
+
     graphs_dir_path = os.path.join(artifacts_dir,reports_dir, graphs_dir)
     model_dir_path = os.path.join(artifacts_dir, model_dir)
     trained_model_graph_dir_path = os.path.join(artifacts_dir,reports_dir, trained_model_graph_dir)
@@ -85,7 +127,7 @@ if __name__=="__main__":
         logging.info("starting STAGE 05 TESTING MODEL ACCURACY")
         create_directory([os.path.join(artifacts_dir,reports_dir, trained_model_graph_dir)])
         model = load_model(saved_model_file_path)
-        model_testing(model=model, test_data_file=test_data_file_path, graphs_dir_path= trained_model_graph_dir_path)
+        model_testing(model=model, test_data_file=test_data_file_path, graphs_dir_path= trained_model_graph_dir_path, scores_filepath=scores_filepath)
         logging.info("Stage 05 completed successfully and all the data is saved in the locals")
     except Exception as e:
         logging.info(e)
